114db0cf78463a1face8c5b564740dc29344eab2
rafaelsantos <rafael.balieiro@luizalabs.com>
2020-10-08 13:42:52 -0300

    Using loops to fetch all array data

--- customers.js
+++ customers.js
@@ -2,15 +2,28 @@ const customers = [
     {
         cpf: 18875539081,
         name: "Mark"
+    },
+    {
+        cpf: 18867539081,
+        name: "Paul"
     }
 ]
 
-const content = `
-<tr>
-<td>${customers[0].cpf}</td>
-<td>${customers[0].name}</td>
-</tr>
-`
 const tableBody = document.querySelector("[data-content-table]")
 
-tableBody.innerHTML = content
+const showCustomers = (cpf, name) => {
+    const line = document.createElement('tr')
+
+    const content = `
+        <td>${cpf}</td>
+        <td>${name}</td>
+    `
+
+    line.innerHTML = content
+
+    return line
+}
+
+customers.forEach(customer => {
+    tableBody.appendChild(showCustomers(customer.cpf, customer.name))
+})
