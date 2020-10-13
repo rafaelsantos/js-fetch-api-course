eef0ed71ea3401b8ca2b8cbb9193f554bac6966b
rafaelsantos <rafael.balieiro@luizalabs.com>
2020-10-13 13:17:19 -0300

    Creating post method in API for JS

--- api/client.js
+++ api/client.js
@@ -1,11 +1,30 @@
-let getCustomers = () => {
-    return fetch("http://localhost:4000/clientes", {
+const url = "http://localhost:4000/clientes"
+
+const getCustomers = () => {
+    return fetch(url, {
       method: "get"
     })
-      .then(resposta => {
-        return resposta.json();
+      .then(response => {
+        return response.json();
       })
       .then(json => {
         return json;
       });
   };
+
+const saveCustomer = (nome, cpf) => {
+  const json = JSON.stringify({
+    nome: nome,
+    cpf: cpf
+  })
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: json
+  })
+  .then(response => {
+    return response.body
+  })
+}
